7dff765ba292cef633e2d0c7d86303024797ee2f
Valdir Júnior <valdirmsjunior@gmail.com>
2024-02-08 16:33:13 -0300

    reafatorando as rotas invoice

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function() {
     Route::get('/users', [UserController::class, 'index']);
     Route::get('/users/{user}', [UserController::class, 'show']);
-    Route::get('/invoices', [InvoiceController::class, 'index']);
-    Route::get('/invoices/{invoice}', [InvoiceController::class, 'show']);
-    Route::post('/invoices', [InvoiceController::class, 'store']);
-    Route::put('/invoices/{invoice}', [InvoiceController::class, 'update']);
-    Route::delete('/invoices/{invoice}', [InvoiceController::class, 'destroy']);
+    
+    Route::apiResource('invoices', InvoiceController::class);
+    // Route::get('/invoices', [InvoiceController::class, 'index']);
+    // Route::get('/invoices/{invoice}', [InvoiceController::class, 'show']);
+    // Route::post('/invoices', [InvoiceController::class, 'store']);
+    // Route::put('/invoices/{invoice}', [InvoiceController::class, 'update']);
+    // Route::delete('/invoices/{invoice}', [InvoiceController::class, 'destroy']);
 });
 
