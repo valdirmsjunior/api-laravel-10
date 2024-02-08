86d1e4687512199ceaf381e7ec71670e5d5527f3
Valdir Júnior <valdirmsjunior@gmail.com>
2024-02-08 16:14:07 -0300

    add update in invoice

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -55,9 +55,35 @@ class InvoiceController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Invoice $invoice)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'type' => 'required|max:1|in:'.implode(',', [ 'B', 'C', 'P']),
+            'paid' => 'required|numeric|between:0,1',
+            'value' => 'required|numeric',
+            'payment_date' => 'nullable|date_format:Y-m-d H:i:s'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error('Validation failed', 422, $validator->errors());
+        }
+
+        $validated = $validator->validated();
+
+        $updated = $invoice->update([
+            'user_id' => $validated['user_id'],
+            'type' => $validated['type'],
+            'paid' => $validated['paid'],
+            'value' => $validated['value'],
+            'payment_date' => $validated['paid'] ? $validated['payment_date'] : null
+        ]);
+
+        if ($updated) {
+            return $this->response('Invoice updated', 200, new InvoiceResource($invoice->load('user')));
+        }
+
+        return $this->error('Invoice not updated', 400);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,6 @@ Route::prefix('v1')->group(function() {
     Route::get('/invoices', [InvoiceController::class, 'index']);
     Route::get('/invoices/{invoice}', [InvoiceController::class, 'show']);
     Route::post('/invoices', [InvoiceController::class, 'store']);
+    Route::put('/invoices/{invoice}', [InvoiceController::class, 'update']);
 });
 
