be1cc467fb4e8761f1d33bd7ce11affd75d16a7d
Valdir Júnior <valdirmsjunior@gmail.com>
2024-02-08 16:28:00 -0300

    add show invoice

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -47,9 +47,9 @@ class InvoiceController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Invoice $invoice)
     {
-        //
+        return new InvoiceResource($invoice);
     }
 
     /**
